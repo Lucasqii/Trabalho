206e76e03601b11f4ee79138f286e2acd632b6d0
Lucasqii <lucadhenrcosta@gmail.com>
2025-07-05 12:13:08 -0300

    Add files via upload

---
+++ Trabalho/script.js
@@ -0,0 +1,7 @@
+let botao = document.querySelector("#botao");
+
+botao.addEventListener("click",trocaTexto) /* Quando clica no botão, adiciona uma frase abaixo dele */
+
+function trocaTexto(){ /* Frase que vai ser adicionada */
+    para2.innerHTML='A melhor forma de prever o futuro é criá-lo.'
+}
